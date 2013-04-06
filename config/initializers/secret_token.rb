5de36172a9707f32085dd1726da7865fc7921e12
Michael Makarov <info1117@gmail.com>
2013-04-06 13:15:01 -0700

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Comedyhack::Application.config.secret_token = 'b17530c0d69b9d11e0506b664d650e20df2f5e5304cd22a94e3b3aad88275a0048e652458672e6f45be7f3002a00d1f1e52f699b6b0404926d5967ae20cfa200'
