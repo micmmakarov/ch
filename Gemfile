db5f3126e0770f2296f0652cd7cf3657dd0db60c
Vasily Myazin <vm@konspiredesign.com>
2013-04-06 14:40:13 -0700

    Added Bootstrap library

--- Gemfile
+++ Gemfile
@@ -7,6 +7,11 @@ gem 'rails', '3.2.12'
 
 gem 'sqlite3'
 
+# sass mixin library
+gem 'bourbon'
+
+# bootstrap style library
+gem 'bootstrap-sass'
 
 # Gems used only for assets and not required
 # in production environments by default.
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130406210203) do
+ActiveRecord::Schema.define(:version => 20130406211926) do
 
   create_table "authentications", :force => true do |t|
     t.integer  "user_id"
@@ -19,6 +19,15 @@ ActiveRecord::Schema.define(:version => 20130406210203) do
     t.string   "uid"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+  end
+
+  create_table "events", :force => true do |t|
+    t.string   "title"
+    t.text     "description"
+    t.datetime "start"
+    t.integer  "user_id"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
   end
 
   create_table "users", :force => true do |t|
