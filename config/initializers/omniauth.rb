c862a97170ee036ecaed4c836dee0e8a6fdc9da8
Michael Makarov <info1117@gmail.com>
2013-04-07 14:23:56 -0700

    Facebook production

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -6,6 +6,6 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   #development
   provider :facebook, '222424071230782', '902cce4522a3185b00a9bf7fa83f5334' if Rails.env.development?
   #production
-  provider :facebook, '157502194406861', 'a38f27ac0ffe39e1f91c4efa45991ea6' if Rails.env.production?
+  provider :facebook, '460069040739570', '560cdd572efd9d5d5076c46e972a35f4' if Rails.env.production?
 
 end
