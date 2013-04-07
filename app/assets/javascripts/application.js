839e01b7730dc3ed064c3b4ebfa30885e7a62ed2
Vasily Myazin <vm@konspiredesign.com>
2013-04-06 17:09:56 -0700

    Added basic Angular routing and partials

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,7 +12,8 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require_tree .
 //= require angular.min
-//= require angle-up
+//= require angular-resource
+//? require angle-up
+//= require_tree .
 //= require_tree ./angular
