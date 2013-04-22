1c4c709b1516c3ed3395178b6c3bf9e798394bdf
Michael Makarov <info1117@gmail.com>
2013-04-22 10:56:04 -0700

    Quick fix

--- app/controllers/api/venues_controller.rb
+++ app/controllers/api/venues_controller.rb
@@ -6,17 +6,18 @@ class Api::VenuesController < ApplicationController
       query = params[:query]
     end
     if query.map {|k,v| v}.join.length > 0
-      if query['place_id'].present?
-        place = Place.find(query['place_id'])
+      if query[:place_id].present?
+        place = Place.find(query[:place_id])
         query[:state] = place.state
         query[:city] = place.city
-        query.delete 'place_id'
+        query.delete :place_id
       end
       @venues = Venue.all
-      #query.each do |key, value|
-      #  puts "Ok, lets go: #{key}, #{value}"
-      #  @venues.select! {|v| v[key].upcase.include? value.upcase}
-      #end
+      query.each do |key, value|
+        @venues.select! do |v|
+          v[key].upcase.include? value.upcase if v[key].present?
+        end
+      end
       render json: @venues.to_json(include_hash)
     else
       render json: []
