01d46011c10122669d443882e5d349bc2e9350ae
Michael Makarov <info1117@gmail.com>
2013-04-21 20:47:16 -0700

    Fixes a bug

--- app/controllers/api/home_controller.rb
+++ app/controllers/api/home_controller.rb
@@ -2,6 +2,7 @@ class Api::HomeController < ApplicationController
   def index
     offset = rand(User.count)
     featured_users = User.all(:offset => offset, :limit => 3)
+    featured_users = User.all(:limit => 3) if featured_users.length < 3
     render json: {featured_users: featured_users.as_json(include_hash)}
   end
 private
